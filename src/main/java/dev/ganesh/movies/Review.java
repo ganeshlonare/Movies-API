300bc0d74783ed0fdb331683956c421e27b8b4b7
ganeshlonare <ganeshlonare311@gmail.com>
2024-03-31 13:25:11 +0530

    Created Review entity class.

---
+++ src/main/java/dev/ganesh/movies/Review.java
@@ -0,0 +1,20 @@
+package dev.ganesh.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+}
