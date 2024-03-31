f04ca8743c9d3c1e62b68313d0a79499329b82b6
ganeshlonare <ganeshlonare311@gmail.com>
2024-03-31 15:23:17 +0530

    Added getSingleMovie API

--- src/main/java/dev/ganesh/movies/MovieService.java
+++ src/main/java/dev/ganesh/movies/MovieService.java
@@ -1,11 +1,13 @@
 package dev.ganesh.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,9 @@ public class MovieService {
 
     public List<Movie> allMovies(){
         return movieRepositary.findAll();
+    }
+
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return movieRepositary.findMovieByImdbId(imdbId);
     }
 }
